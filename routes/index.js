633f5abfb0aa26ea542a2d09f3372872fe58b93f
Garrett Kucinski <garrettryan@doejo.com>
2017-06-05 23:09:38 -0500

    added more content

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    const date = new Date();
+    const currentTime = date.toTimeString();
     const friends = req.app.get('friends');
     const timeline = req.app.get('timeline');
-    res.render('index', { friends: friends, timeline: timeline });
+    res.render('index', { friends: friends, timeline: timeline, currentTime: currentTime });
 });
 
 module.exports = router;
